79c155dc0d1e121f965fbf5b4c48a23f182d47b3
Motestt <joao.sales10@hotmail.com>
2024-10-29 09:31:13 -0300

    Padronizar botões

--- FazendaUrbana/Entities/Forms/AdicionarRelatoriocs.Designer.cs
+++ FazendaUrbana/Entities/Forms/AdicionarRelatoriocs.Designer.cs
@@ -50,9 +50,10 @@
             this.label1.BackColor = System.Drawing.Color.YellowGreen;
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(168, 45);
+            this.label1.Location = new System.Drawing.Point(126, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 28);
+            this.label1.Size = new System.Drawing.Size(84, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Preencher";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -63,9 +64,10 @@
             this.label2.BackColor = System.Drawing.Color.YellowGreen;
             this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(485, 45);
+            this.label2.Location = new System.Drawing.Point(364, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(185, 28);
+            this.label2.Size = new System.Drawing.Size(151, 21);
             this.label2.TabIndex = 1;
             this.label2.Text = "Dados do Relatorio";
             // 
@@ -75,9 +77,10 @@
             this.label3.BackColor = System.Drawing.Color.YellowGreen;
             this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(98, 135);
+            this.label3.Location = new System.Drawing.Point(74, 110);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 28);
+            this.label3.Size = new System.Drawing.Size(43, 21);
             this.label3.TabIndex = 2;
             this.label3.Text = "Tipo";
             // 
@@ -87,9 +90,10 @@
             this.label4.BackColor = System.Drawing.Color.YellowGreen;
             this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
-            this.label4.Location = new System.Drawing.Point(101, 192);
+            this.label4.Location = new System.Drawing.Point(76, 156);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 28);
+            this.label4.Size = new System.Drawing.Size(40, 21);
             this.label4.TabIndex = 3;
             this.label4.Text = "Ano";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -100,55 +104,62 @@
             this.label5.BackColor = System.Drawing.Color.YellowGreen;
             this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
-            this.label5.Location = new System.Drawing.Point(97, 246);
+            this.label5.Location = new System.Drawing.Point(73, 200);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(82, 28);
+            this.label5.Size = new System.Drawing.Size(67, 21);
             this.label5.TabIndex = 4;
             this.label5.Text = "Periodo";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(202, 135);
+            this.comboBox1.Location = new System.Drawing.Point(152, 110);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.Size = new System.Drawing.Size(92, 21);
             this.comboBox1.TabIndex = 5;
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(202, 192);
+            this.comboBox2.Location = new System.Drawing.Point(152, 156);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.Size = new System.Drawing.Size(92, 21);
             this.comboBox2.TabIndex = 6;
             // 
             // comboBox3
             // 
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(202, 250);
+            this.comboBox3.Location = new System.Drawing.Point(152, 203);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.Size = new System.Drawing.Size(92, 21);
             this.comboBox3.TabIndex = 7;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(509, 192);
+            this.textBox1.Location = new System.Drawing.Point(382, 156);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(161, 22);
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
             this.textBox1.TabIndex = 8;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(509, 135);
+            this.textBox2.Location = new System.Drawing.Point(382, 110);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(161, 22);
+            this.textBox2.Size = new System.Drawing.Size(122, 20);
             this.textBox2.TabIndex = 9;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(509, 253);
+            this.textBox3.Location = new System.Drawing.Point(382, 206);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(161, 22);
+            this.textBox3.Size = new System.Drawing.Size(122, 20);
             this.textBox3.TabIndex = 10;
             // 
             // button1
@@ -157,9 +168,10 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
             this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(713, 400);
+            this.button1.Location = new System.Drawing.Point(535, 325);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 38);
+            this.button1.Size = new System.Drawing.Size(56, 31);
             this.button1.TabIndex = 11;
             this.button1.Text = "Voltar";
             this.button1.UseVisualStyleBackColor = false;
@@ -169,9 +181,10 @@
             this.button2.BackColor = System.Drawing.Color.YellowGreen;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
-            this.button2.Location = new System.Drawing.Point(11, 400);
+            this.button2.Location = new System.Drawing.Point(8, 325);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(153, 38);
+            this.button2.Size = new System.Drawing.Size(115, 31);
             this.button2.TabIndex = 12;
             this.button2.Text = "Configurações";
             this.button2.UseVisualStyleBackColor = false;
@@ -181,9 +194,10 @@
             this.button3.BackColor = System.Drawing.Color.YellowGreen;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
-            this.button3.Location = new System.Drawing.Point(560, 304);
+            this.button3.Location = new System.Drawing.Point(420, 247);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(110, 41);
+            this.button3.Size = new System.Drawing.Size(82, 33);
             this.button3.TabIndex = 13;
             this.button3.Text = "Imprimir";
             this.button3.UseVisualStyleBackColor = false;
@@ -191,7 +205,7 @@
             // 
             // AdicionarRelatoriocs
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(800, 450);
@@ -209,6 +223,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AdicionarRelatoriocs";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AdicionarRelatoriocs";
